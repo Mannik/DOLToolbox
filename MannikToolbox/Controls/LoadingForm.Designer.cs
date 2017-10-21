d982ec7b00c0a846b4868b22300a2f759dd761d9
Lucas Falk <LRFalk01@gmail.com>
2017-10-21 01:27:07 -0400

    Wire in Spell Tab
    Wire in Spell tab lookup, update, insert.
    Register objects on form load and add progress loading bar.

---
+++ MannikToolbox/Controls/LoadingForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MannikToolbox.Controls
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 150);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(150, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(3, 117);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(0, 13);
+            this.lblText.TabIndex = 2;
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 173);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "LoadingForm";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblText;
+    }
+}
--- MannikToolbox/Controls/SpellControl.cs
+++ MannikToolbox/Controls/SpellControl.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using DOL.Database;
 using MannikToolbox.Forms;
@@ -11,14 +10,12 @@ namespace MannikToolbox.Controls
     public partial class SpellControl : UserControl
     {
         private readonly SpellService _spellService;
-        private readonly ModelImageService _modelImageService;
         private DBSpell _spell;
 
         public SpellControl()
         {
             InitializeComponent();
             _spellService = new SpellService();
-            _modelImageService = new ModelImageService();
         }
 
         private void SpellControl_Load(object sender, EventArgs e)
@@ -28,28 +25,14 @@ namespace MannikToolbox.Controls
             SetupDropdowns();
         }
 
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            var dialog = new InputDialogBox
-            {
-                Caption = { Text = @"Please enter Spell ID" }
-            };
-
-            if (dialog.ShowDialog(this) == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.Input.Text))
-            {
-                LoadSpell(dialog.Input.Text);
-            }
-            dialog.Dispose();
-        }
-
-        private void LoadSpell(string spellId)
+        private async Task LoadSpell(string spellId)
         {
             if (string.IsNullOrWhiteSpace(spellId))
             {
                 return;
             }
 
-            _spell = _spellService.GetSpell(spellId);
+            _spell = await _spellService.Get(spellId);
 
             if (_spell == null)
             {
@@ -65,20 +48,60 @@ namespace MannikToolbox.Controls
         private void SetupDropdowns()
         {
             ComboboxService.BindWeaponDamageTypes(_DamageType);
-            ComboboxService.BindTargets(_Target);
+            ComboboxService.BindTargets(Target);
             ComboboxService.BindInstrumentRequirements(_InstrumentRequirement);
         }
 
         private void Insertspell_Click(object sender, EventArgs e)
+        {
+            _spell = new DBSpell
+            {
+                AllowAdd = true
+            };
+
+            SyncSpell();
+
+            _spell.ObjectId = null;
+            _spell.SpellID = _spellService.GetNextSpellId();
+
+            _spellService.Save(_spell);
+            BindingService.ClearData(this);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new InputDialogBox
+            {
+                Caption = { Text = @"Please enter Spell ID" }
+            };
+
+            if (dialog.ShowDialog(this) == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.Input.Text))
+            {
+                await LoadSpell(dialog.Input.Text);
+            }
+            dialog.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
             if (_spell == null)
             {
                 return;
             }
 
-            BindingService.SyncData(_spell, this);
+            SyncSpell();
+            _spellService.Save(_spell);
+            BindingService.ClearData(this);
+        }
 
-            _spellService.SaveSpell(_spell);
+        private void SyncSpell()
+        {
+            BindingService.SyncData(_spell, this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BindingService.ClearData(this);
         }
     }
 }
