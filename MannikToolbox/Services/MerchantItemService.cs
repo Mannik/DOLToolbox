fbda7ed734fda140891cf53c1fe4d42b4951cb74
Lucas Falk <LRFalk01@gmail.com>
2017-10-28 15:15:19 -0400

    Merchant tab refactor
    Allow editing of ItemListID
    Make it clearer how to start a new list
    Auto condence an item list to remove missing slots
    Showing loading form when saving (because new opperations can save many files)

--- MannikToolbox/Services/MerchantItemService.cs
+++ MannikToolbox/Services/MerchantItemService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using DOL.Database;
 using DOL.Database.UniqueID;
 using DOL.GS;
@@ -15,45 +16,55 @@ namespace MannikToolbox.Services
                 .ToList();
         }
 
-        public string Save(List<MerchantItem> models)
+        public async Task<string> Save(List<MerchantItem> models, string itemListId)
         {
-            var templateId = models.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x.ItemListID))?.ItemListID;
-
-            List<MerchantItem> current = new List<MerchantItem>();
-            if (!string.IsNullOrWhiteSpace(templateId))
+            return await Task.Run(() =>
             {
-                current = Get(templateId);
-            }
-            else
-            {
-                templateId = IDGenerator.GenerateID();
-            }
+                var existingId = models.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x.ItemListID))?.ItemListID;
 
-            // remove deleted
-            current
-                .Where(x => !models.Select(s => s.ObjectId).Contains(x.ObjectId))
-                .ForEach(x => DatabaseManager.Database.DeleteObject(x));
-
-            // add new
-            models
-                .Where(x => string.IsNullOrWhiteSpace(x.ItemListID))
-                .ForEach(x =>
+                List<MerchantItem> current = new List<MerchantItem>();
+                if (!string.IsNullOrWhiteSpace(existingId))
                 {
-                    x.ItemListID = templateId;
-                    DatabaseManager.Database.AddObject(x);
-                });
+                    current = Get(existingId);
+                }
 
-            // update changed
-            (from model in models
-                    join existing in current on model.ObjectId equals existing.ObjectId
-                    where model.ItemTemplateID != existing.ItemTemplateID ||
-                          model.PageNumber != existing.PageNumber ||
-                          model.SlotPosition != existing.SlotPosition
-                    select model)
-                .ToList()
-                .ForEach(x => DatabaseManager.Database.SaveObject(x));
+                if (string.IsNullOrWhiteSpace(itemListId))
+                {
+                    itemListId = IDGenerator.GenerateID();
+                }
 
-            return templateId;
+                // update all items to the set ID
+                models
+                    .Where(x => !string.IsNullOrWhiteSpace(x.ItemListID))
+                    .ForEach(x => x.ItemListID = itemListId);
+
+                // remove deleted
+                current
+                    .Where(x => !models.Select(s => s.ObjectId).Contains(x.ObjectId))
+                    .ForEach(x => DatabaseManager.Database.DeleteObject(x));
+
+                // add new
+                models
+                    .Where(x => string.IsNullOrWhiteSpace(x.ItemListID))
+                    .ForEach(x =>
+                    {
+                        x.ItemListID = itemListId;
+                        DatabaseManager.Database.AddObject(x);
+                    });
+
+                // update changed
+                (from model in models
+                        join existing in current on model.ObjectId equals existing.ObjectId
+                        where model.ItemListID != existing.ItemListID ||
+                              model.ItemTemplateID != existing.ItemTemplateID ||
+                              model.PageNumber != existing.PageNumber ||
+                              model.SlotPosition != existing.SlotPosition
+                        select model)
+                    .ToList()
+                    .ForEach(x => DatabaseManager.Database.SaveObject(x));
+
+                return itemListId;
+            });
         }
     }
 }
