071dff1b270ed910c0260554e667a2778fc492b2
DigitalBox98 <57635141+DigitalBox98@users.noreply.github.com>
2021-11-04 22:42:46 +0100

    DOLToolbox : attempt to recognize existing Dataquest (#73)
    * DOLToolbox : attempt to recognize existing Dataquest
    
    * DOLToolbox : Save quest info is now implemented
    
    * DOLToolbox : minor code clean up
    
    * DOLToolbox : fix on step type replacement
    
    * Fix on dataquest save
    
    * DOLToolBox : Allowed Classes field is now useable
    
    * DOLToolBox : Allowed Classes field with All option
    
    * DOLToolBox : Quest Dependency field is now recognized
    
    Co-authored-by: DigitalBox98 <digitalbox098@gmail.com>

---
+++ DOLToolbox/Services/DataQuestService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DOL.Database;
+using DOL.Database.UniqueID;
+
+namespace DOLToolbox.Services
+{
+    public class DataQuestService
+    {
+        public async Task<DBDataQuest> Get(string objectId)
+        {
+            return await Task.Run(() => DatabaseManager.Database.FindObjectByKey<DBDataQuest>(objectId) ??
+                                        DatabaseManager.Database.SelectObjects<DBDataQuest>("`ID` = @Id",
+                                            new QueryParameter("@Id", objectId)).FirstOrDefault());
+        }
+
+        public async Task<List<DBDataQuest>> Get()
+        {
+            return await Task.Run(() => DatabaseManager.Database.SelectAllObjects<DBDataQuest>().ToList());
+        }
+
+        public string Save(DBDataQuest quest)
+        {
+            if (!quest.IsPersisted)
+            {
+                quest.ObjectId = IDGenerator.GenerateID();
+                DatabaseManager.Database.AddObject(quest);
+                return quest.ObjectId;
+            }
+
+            DatabaseManager.Database.SaveObject(quest);
+            return quest.ObjectId;
+        }
+
+        public int GetNextSpellId()
+        {
+            return DatabaseManager.Database.SelectAllObjects<DBDataQuest>()
+                       .OrderByDescending(x => x.ID)
+                       .Select(x => x.ID)
+                       .FirstOrDefault() + 1;
+        }
+
+        public void Delete(DBDataQuest quest)
+        {
+            DatabaseManager.Database.DeleteObject(quest);
+        }
+    }
+}
